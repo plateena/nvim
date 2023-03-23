828ccd6c1ba1578e91dfd2c18f6572f0d2c387ea
zainundin <dean.coders@gamil.com>
2023-03-23 11:04:55 +0800

    feat(snippet): add vsnip and friendly snip

--- lua/plateena/plugins.lua
+++ lua/plateena/plugins.lua
@@ -1,4 +1,3 @@
-
 local pack = require("plateena.utils.packer-loaded")
 
 return require('packer').startup(function(use)
@@ -9,33 +8,44 @@ return require('packer').startup(function(use)
         "savq/melange-nvim",
         "Shatur/neovim-ayu",
 
-        {'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    },
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "tpope/vim-fugitive",
+        {
+            'nvim-telescope/telescope.nvim',
+            tag = '0.1.1',
+            -- or                            , branch = '0.1.x',
+            requires = { { 'nvim-lua/plenary.nvim' } }
+        },
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        "tpope/vim-fugitive",
 
-    "folke/which-key.nvim",
+        "folke/which-key.nvim",
 
-    'nvim-tree/nvim-web-devicons',
-    'onsails/lspkind.nvim',
+        'nvim-tree/nvim-web-devicons',
+        'onsails/lspkind.nvim',
 
-    -- autocomplete
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip',
-    'jcha0713/cmp-tw2css',
-    'delphinus/cmp-ctags',
-}
+        -- autocomplete
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-vsnip',
+        {
+            'hrsh7th/vim-vsnip',
+            requires = {
+                'hrsh7th/vim-vsnip-integ',
+                "rafamadriz/friendly-snippets",
+            },
+            config = function()
+                vim.cmd("let g:vsnip_snippet_dir='" .. vim.fn.stdpath("config") .. "/snippets'")
+            end
+        },
+        'jcha0713/cmp-tw2css',
+        'delphinus/cmp-ctags',
+    }
 
-if pack.ensure_packer then
-    require('packer').sync()
-end
+    if pack.ensure_packer then
+        require('packer').sync()
+    end
 end)
