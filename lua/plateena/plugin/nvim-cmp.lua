5530ef8ab8231a275cdcc7e8c3a88f5453e0d7db
zainundin <plateena711@gmail.com>
2023-04-05 15:40:15 +0800

    fix(lsp): fix lua lsp

--- lua/plateena/plugin/nvim-cmp.lua
+++ lua/plateena/plugin/nvim-cmp.lua
@@ -213,10 +213,3 @@ cmp.setup.cmdline(":", {
 		{ name = "cmdline" },
 	}),
 })
-
--- Set up lspconfig.
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require("lspconfig")["lua_ls"].setup({
-	capabilities = capabilities,
-})
