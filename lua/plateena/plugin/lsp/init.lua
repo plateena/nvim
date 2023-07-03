ba25cde04f9481a7ddd644bbc295f3f9079542cb
zainundin <plateena711@gmail.com>
2023-07-03 11:14:57 +0800

    fix(lsp): fix json not working

--- lua/plateena/plugin/lsp/init.lua
+++ lua/plateena/plugin/lsp/init.lua
@@ -1,4 +1,5 @@
 require("plateena.plugin.lsp.lspconfig")
+require("plateena.plugin.lsp.null_ls")
 require("plateena.plugin.lsp.mason")
 require("plateena.plugin.lsp.lua")
 require("plateena.plugin.lsp.emmet_ls")
