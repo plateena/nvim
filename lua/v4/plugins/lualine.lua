1d65c42dba95fa58d640bbdd5b6a009070c51e8c
zainundin <zainundin.coders@gmail.com>
2024-10-24 12:59:40 +0800

    feat(lualine): nothing change

--- lua/v4/plugins/lualine.lua
+++ lua/v4/plugins/lualine.lua
@@ -2,18 +2,6 @@ return {
     'nvim-lualine/lualine.nvim',
     config = function()
         local lualine = require("lualine")
-        local trouble = require("trouble")
-        local symbols = trouble.statusline({
-            mode = "lsp_document_symbols",
-            groups = {},
-            title = false,
-            filter = { range = true },
-            format = "{kind_icon}{symbol.name:Normal}",
-            -- The following line is needed to fix the background color
-            -- Set it to the lualine section you want to use
-            hl_group = "lualine_c_normal",
-        })
-
         lualine.setup({
             options = {
                 icons_enabled = true,
@@ -36,9 +24,7 @@ return {
             sections = {
                 lualine_a = { "mode" },
                 lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = { "filename",
-                    { symbols.get, cond = symbols.has },
-                },
+                lualine_c = { "filename" },
                 lualine_x = { "encoding", "fileformat", "filetype" },
                 lualine_y = { "progress" },
                 lualine_z = { "location" },
