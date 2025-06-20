4a1dc57b2d7653c09fd7f9d72fa3649c12b628a2
zainundin <zainundin.coders@gmail.com>
2025-06-20 17:42:45 +0800

    feat(git): improve fugutive mapping

--- lua/v5/plugins/git.lua
+++ lua/v5/plugins/git.lua
@@ -4,12 +4,19 @@ return {
         config = function()
             vim.keymap.set("n", "<Leader>g", "", { desc = "Git" })
             vim.keymap.set("n", "<Leader>gs", "<Cmd>Git<Cr>", { desc = "Git" })
-            vim.keymap.set("n", "<Leader>gl", "<Cmd>Git log<Cr>", { desc = "Git log" })
+            vim.keymap.set("n", "<Leader>gP", "<Cmd>Git pull<Cr>", { desc = "Git pull" })
             vim.keymap.set("n", "<Leader>gv", "<Cmd>Gvdiffsplit<Cr>", { desc = "Git diff split" })
-            vim.keymap.set("n", "<Leader>gh", "<Cmd>diffget //2<Cr>", { desc = "diffget //2" })
-            vim.keymap.set("n", "<Leader>gl", "<Cmd>diffget //3<Cr>", { desc = "diffget //3" })
-            vim.keymap.set("n", "<Leader>gu", "<Cmd>diffput //2<Cr>", { desc = "diffput //2" })
-            vim.keymap.set("n", "<Leader>gi", "<Cmd>diffput //3<Cr>", { desc = "diffput //3" })
+
+            vim.api.nvim_create_autocmd("BufEnter", {
+                callback = function()
+                    if vim.wo.diff then
+                        vim.keymap.set("n", "<Leader>gh", "<Cmd>diffget //2<Cr>", { desc = "diffget //2" })
+                        vim.keymap.set("n", "<Leader>gl", "<Cmd>diffget //3<Cr>", { desc = "diffget //3" })
+                        vim.keymap.set("n", "<Leader>gu", "<Cmd>diffput //2<Cr>", { desc = "diffput //2" })
+                        vim.keymap.set("n", "<Leader>gi", "<Cmd>diffput //3<Cr>", { desc = "diffput //3" })
+                    end
+                end
+            })
         end,
     },
     {
