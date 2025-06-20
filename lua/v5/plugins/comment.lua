a73a584957a775e60f5c3f30e30a0b9c77c0c6d3
zainundin <zainundin.coders@gmail.com>
2025-06-20 16:15:54 +0800

    feat: improve the comment nvim

--- lua/v5/plugins/comment.lua
+++ lua/v5/plugins/comment.lua
@@ -2,14 +2,38 @@ return {
     "numToStr/Comment.nvim",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-        "JoosepAlviste/nvim-ts-context-commentstring",
+        {
+            "JoosepAlviste/nvim-ts-context-commentstring",
+            opts = { enable_autocmd = false },
+        },
     },
     config = function()
         local comment = require("Comment")
-        local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+        local ts_context = require("ts_context_commentstring.integrations.comment_nvim")
 
         comment.setup({
-            pre_hook = ts_context_commentstring.create_pre_hook(),
+            padding = true, -- add space between comment and code
+            sticky = true,  -- keep cursor in place
+            ignore = "^$",  -- ignore empty lines
+            toggler = {
+                line = "gcc",
+                block = "gbc",
+            },
+            opleader = {
+                line = "gc",
+                block = "gb",
+            },
+            extra = {
+                above = "gcO",
+                below = "gco",
+                eol   = "gcA",
+            },
+            mappings = {
+                basic = true,
+                extra = true,
+            },
+            pre_hook = nil,
+            post_hook = nil,
         })
     end,
 }
