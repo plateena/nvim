9ba5825324c0d404ebe4b35bbb9ba24efa70ba67
zainundin <mzd@inscale.net>
2026-05-25 13:47:44 +0800

    feat(flash): enable jump labels and search mode

--- lua/v6/plugins/flash.lua
+++ lua/v6/plugins/flash.lua
@@ -2,15 +2,43 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   keys = {
-    { "s", function() require("flash").jump() end, mode = { "n", "x", "o" }, desc = "Flash jump" },
-    { "S", function() require("flash").treesitter() end, mode = { "n", "x", "o" }, desc = "Flash treesitter" },
-    { "r", function() require("flash").remote() end, mode = "o", desc = "Remote flash" },
-    { "<c-s>", function() require("flash").toggle() end, mode = "c", desc = "Toggle flash search" },
+    {
+      "s",
+      function()
+        require("flash").jump()
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Flash jump",
+    },
+    {
+      "S",
+      function()
+        require("flash").treesitter()
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Flash treesitter",
+    },
+    {
+      "r",
+      function()
+        require("flash").remote()
+      end,
+      mode = "o",
+      desc = "Remote flash",
+    },
+    {
+      "<c-s>",
+      function()
+        require("flash").toggle()
+      end,
+      mode = "c",
+      desc = "Toggle flash search",
+    },
   },
   opts = {
     modes = {
-      char = { enabled = true },
-      search = { enabled = false },
+      char = { enabled = true, jump_labels = true },
+      search = { enabled = true },
     },
   },
 }
